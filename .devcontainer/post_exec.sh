5317c95040e0a6569f9c694cb67a685aedba0fd0
Fabian Schmidt <fabian.schmidt@hs-esslingen.de>
2024-12-09 19:36:01 +0100

    added devcontainer

---
+++ .devcontainer/post_exec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eux
+
+echo ">>> Start of post install script <<<"
+
+if [[ ! -d "/workspace/MonoGS" ]]; then
+  echo "Cloning MonoGS repository..."
+  
+  cd /workspace
+
+  git clone --recursive https://github.com/iis-esslingen/MonoGS.git
+
+  pip install submodules/diff-gaussian-rasterization
+  pip install submodules/simple-knn
+fi
+
+echo ">>> End of post install script <<<" 
